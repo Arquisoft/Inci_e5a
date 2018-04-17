88fc5329fe088ad675d6003e9dfbe2a74e793535
Jesus Fdez <uo250914@uniovi.es>
2018-04-17 13:02:43 +0200

    Actualizado controlador de agentes

--- src/main/java/asw/inciManager/inciManager_e5a/controller/AgentController.java
+++ src/main/java/asw/inciManager/inciManager_e5a/controller/AgentController.java
@@ -1,12 +1,16 @@
 package asw.inciManager.inciManager_e5a.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import asw.agents.util.Assert;
@@ -22,10 +26,42 @@ import asw.inciManager.inciManager_e5a.services.AgentsService;
 public class AgentController {
 	
 	@Autowired
-	AgentsService agentService; 
+	AgentsService agentsService;
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String inicalicerLogin(Model model) {
+		return "sendIncidence";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String getLogin(HttpSession session, @RequestParam String email, @RequestParam String password, @RequestParam String kind,
+			Model model) {
+
+		Assert.isEmailEmpty(email);
+		Assert.isEmailValid(email);
+		Assert.isPasswordEmpty(password);
+		Assert.isKindNull(kind);
+
+		Agent agent = agentsService.getAgent(email);
+
+		//Assert.isAgentNull(agent);
+		//Assert.isPasswordCorrect(password, agent);
+		//Assert.isKindCorrect(kind,agent);
+
+		session.setAttribute("agent", agent);
+		
+		return "datosAgent";
+	}
+
+	@ExceptionHandler(ErrorResponse.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public String handleErrorResponseNotFound(ErrorResponse excep, Model model) {
+		model.addAttribute("error", excep.getMessageStringFormat());
+
+		return "error";
+	}
 	
-	
+
 	@RequestMapping(value = "/user", method = RequestMethod.POST, headers = { "Accept=application/json",
 			"Accept=application/xml" }, produces = { "application/json", "text/xml" })
 	public ResponseEntity<RespuestaInfoREST> getPOSTpetition(@RequestBody(required = true) PeticionInfoREST peticion) {
@@ -34,7 +70,7 @@ public class AgentController {
 		Assert.isEmailValid(peticion.getLogin());
 		Assert.isPasswordEmpty(peticion.getPassword());
 
-		Agent agent = agentService.getAgent(peticion.getLogin());
+		Agent agent = agentsService.getAgent(peticion.getLogin());
 
 	//	Assert.isAgentNull(agent);
 
@@ -71,11 +107,11 @@ public class AgentController {
 		
 		Assert.isSamePassword(password, newPassword);	
 
-		Agent p = agentService.getAgent(email);
+		Agent p = agentsService.getAgent(email);
 //		Assert.isAgentNull(p);
 //		Assert.isPasswordCorrect(password, p);
 
-		agentService.updatePassword(p, password, newPassword);
+		agentsService.updatePassword(p, password, newPassword);
 
 		RespuestaChangeInfoREST res = new RespuestaChangeInfoREST(email, "contraseña actualizada correctamente");
 		return new ResponseEntity<RespuestaChangeInfoREST>(res, HttpStatus.OK);
@@ -98,11 +134,11 @@ public class AgentController {
 
 		Assert.isPasswordEmpty(password);
 		
-		Agent p = agentService.getAgent(email);
+		Agent p = agentsService.getAgent(email);
 //		Assert.isAgentNull(p);
 //		Assert.isPasswordCorrect(password, p);
 		
-		agentService.updateEmail(p, nuevoEmail);
+		agentsService.updateEmail(p, nuevoEmail);
 
 		RespuestaChangeInfoREST res = new RespuestaChangeInfoREST(nuevoEmail, "email actualizado correctamente");
 		return new ResponseEntity<RespuestaChangeInfoREST>(res, HttpStatus.OK);
@@ -113,6 +149,47 @@ public class AgentController {
 //	public String handleErrorResponses(ErrorResponse error) {
 //		return error.getMessageJSONFormat();
 //	}
+	
+	@RequestMapping(value = "/changeInfo", method = RequestMethod.POST)
+	public String changeInfo() {
+		return "changeInfo";
+	}
 
+	@RequestMapping(value = "/confirmPassword", method = RequestMethod.POST)
+	public String changePassword(HttpSession session, @RequestParam String password,
+			@RequestParam String newPassword, Model model) {
+		Assert.isPasswordEmpty(password);
+		Assert.isPasswordEmpty(newPassword);
+		Assert.isSamePassword(password, newPassword);
 
+		// Agent que se ha logeado antes
+		Agent p = (Agent) session.getAttribute("agent");
+	//	Assert.isAgentNull(p);
+	//	Assert.isPasswordCorrect(password, p);
+
+		// Actualizo sus datos
+		agentsService.updatePassword(p, password, newPassword);
+
+		// Mensaje a mostrar en HTML
+		model.addAttribute("info", "Contraseña actualizada correctamente");
+		return "datosAgent";
+	}
+	
+	@RequestMapping(value = "/confirmEmail", method = RequestMethod.POST)
+	public String changeEmail(HttpSession session, @RequestParam String email, Model model) {
+		Assert.isEmailEmpty(email);
+		Assert.isEmailValid(email);
+
+		// Agent que se ha logeado antes
+		Agent p = (Agent) session.getAttribute("agent");
+//		Assert.isAgentNull(p);
+// ->	Assert.isSameEmail(email, p.getEmail());
+
+		// Actualizo sus datos
+		agentsService.updateEmail(p, email);
+
+		// Mensaje a mostrar en HTML
+		model.addAttribute("info", "Email actualizado correctamente");
+		return "datosAgent";
+	}
 }
