04960221a7aa88dedd57d9387335ac742081f697
Miguel <miguelvilla997@gmail.com>
2018-04-25 13:39:17 +0200

    Eliminación de parte de código muerto y de todas las clases
    manejadoras de errores, ya que se harán en los propios HTML

--- src/main/java/asw/inciManager/inciManager_e5a/controller/CreateIncidence.java
+++
@@ -1,8 +0,0 @@
-package asw.inciManager.inciManager_e5a.controller;
-
-import asw.inciManager.inciManager_e5a.entities.Incidence;
-
-public interface CreateIncidence {
-
-	Incidence createIncidence(String user, String pass, String name, String description);
-}
--- src/main/java/asw/inciManager/inciManager_e5a/repositories/Prueba.java
+++
@@ -1,5 +0,0 @@
-package asw.inciManager.inciManager_e5a.repositories;
-
-public class Prueba {
-
-}
