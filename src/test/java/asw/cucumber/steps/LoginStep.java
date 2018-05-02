05fad34ec1cde9664adae4472c93e7aa1a411c5d
guillediaz11 <guilledp11@gmail.com>
2018-05-02 19:11:15 +0200

    terminada prueba login cucumber

--- src/test/java/asw/cucumber/steps/LoginStep.java
+++ src/test/java/asw/cucumber/steps/LoginStep.java
@@ -1,30 +1,56 @@
 package asw.cucumber.steps;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import asw.inciManager.inciManager_e5a.entities.Agent;
+import asw.inciManager.inciManager_e5a.services.AgentsService;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.es.Entonces;
+import cucumber.api.java.es.Y;
 
 public class LoginStep {
-	
-	
+
 	private String agenteEmail;
 	private String agenteContraseña;
 	private String agenteTipo;
+	
+	@Autowired AgentsService agentsService;
 
-	@Given("^El agente con email \"([^\"]*)\"$")
-	public void el_agente_con_email(String agenteEmail) throws Throwable{
-	      System.out.println("El email del agente es: " + agenteEmail);	
+	@Dado("^El agente con email \"([^\"]*)\"$")
+	public void el_agente_con_email(String agenteEmail) throws Throwable {
+		this.agenteEmail = agenteEmail;
+		System.out.println("El email del agente es: " + agenteEmail);
+	}
+
+	@Y("^Contraseña del angente \"([^\"]*)\"$")
+	public void contraseña_del_agente(String agenteContraseña) throws Throwable {
+		this.agenteContraseña = agenteContraseña;
+		System.out.println("La contraseña del agente es: " + agenteContraseña);
+	}
+
+	@Y("^Tipo de angente \"([^\"]*)\"$")
+	public void tipo_de_agente(String agenteTipo) throws Throwable {
+		this.agenteTipo = agenteTipo;
+		System.out.println("El tipo de agente es: " + agenteTipo);
 	}
 	
-	@And("^Contraseña del angente \"([^\"]*)\"$")
-	public void contraseña_del_agente(String agenteContraseña) throws Throwable{
-	      System.out.println("La contraseña del agente es: " + agenteContraseña);	
+	@Cuando("^introduzco los datos en el formulario$")
+	public void introduzco_los_datos_en_el_formulario() {
+		System.out.println("Introduciendo datos en el formulario...");
 	}
-	@And("^Tipo de angente \"([^\"]*)\"$")
-	public void tipo_de_agente(String agenteTipo) throws Throwable{
-	      System.out.println("El tipo de agente es: " + agenteTipo);	
+	
+	@Y("^presiono el boton de iniciar sesion")
+	public void presiono_el_boton_de_iniciar_sesion() {
+		System.out.println("Presionando botón de iniciar sesión");
+	}
+	
+	@Entonces("^se inicia sesión$") 
+	public void se_inicia_sesion() {
+		Agent agent = agentsService.getAgent(agenteEmail);
+		if (agent != null && agent.getPassword().equals(agenteContraseña) && agent.getKind().equals(agenteTipo))
+			System.out.println("Se ha iniciado sesión correctamente");
+		else System.out.println("No se ha iniciado sesión");
 	}
 
 }
--- src/test/resources/cucumber/login.feature
+++ src/test/resources/cucumber/login.feature
@@ -1,9 +1,10 @@
 #language: es
 Feature: iniciar sesion
 	
-	Scenario: agente tipo persona inicia sesion
-		Given: el agente con email 'guille@uniovi.es'
-			And: y contraseña 'Contra'
-			And: y tipo 'Persona'
-		When el agente presiona el botón de iniciar sesión
-		Then: se inicia sesión
+	Escenario: agente tipo persona inicia sesion
+		Dado: el agente con email 'guille@uniovi.es'
+			Y: y contraseña 'Contra'
+			Y: y tipo 'Persona'
+		Cuando introduzco los datos en el formulario
+			Y: presiono el boton de iniciar sesion
+		Entonces: se inicia sesión
