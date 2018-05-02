0d2c82cf28b5daed44206b73182767dc76804ac6
guillediaz11 <guilledp11@gmail.com>
2018-05-03 00:01:29 +0200

    historia de enviar incidencia hecha en cucumber

--- src/test/java/asw/cucumber/steps/EnviarIncidenciaStep.java
+++ src/test/java/asw/cucumber/steps/EnviarIncidenciaStep.java
@@ -1,8 +1,11 @@
 package asw.cucumber.steps;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import asw.inciManager.inciManager_e5a.entities.Agent;
+import asw.inciManager.inciManager_e5a.entities.Incidence;
 import asw.inciManager.inciManager_e5a.services.AgentsService;
 import asw.inciManager.inciManager_e5a.services.IncidenceService;
 import cucumber.api.java.es.Cuando;
@@ -16,17 +19,13 @@ public class EnviarIncidenciaStep {
 	@Autowired AgentsService agentsService;
 
 	
-	private String agenteLoggeado;
+	private Agent agenteLoggeado;
 	private String nombreIncidencia;
 	private String descripcionIncidencia;
 	private String etiquitasIncidencia;
-
-
-	
-
 	
 	@Dado("^La incidencia con agente loggeado \"([^\"]*)\"$")
-	public void la_incidencia_con_agente_loggeado(String agenteLoggeado) throws Throwable {
+	public void la_incidencia_con_agente_loggeado(Agent agenteLoggeado) throws Throwable {
 		this.agenteLoggeado = agenteLoggeado;
 		System.out.println("El agente que envia la incidencia es: " + agenteLoggeado);
 	}
@@ -48,5 +47,25 @@ public class EnviarIncidenciaStep {
 		this.etiquitasIncidencia = etiquitasIncidencia;
 		System.out.println("El nombre de la incidencia es: " + etiquitasIncidencia);
 	}
+	
+	@Cuando("^introduzco los datos en el formulario$")
+	public void introduzco_los_datos_en_el_formulario() {
+		System.out.println("Introduciendo los datos en el formulario");
+	}
+	
+	@Y("^presiono el boton de enviar incidencia")
+	public void presiono_boton_enviar_incidencia() {
+		System.out.println("Presionando boton de enviar incidencia");
+	}
+	
+	@Entonces("^se envia la incidencia correctamente$")
+	public void se_envia_la_incidencia() {
+		
+		Incidence incidence = new Incidence(null, nombreIncidencia,descripcionIncidencia,agenteLoggeado,Arrays.asList(etiquitasIncidencia.split(",")) );
+		incidenceService.sendIncidence(incidence);
+		System.out.println("Se envía la incidencia correctamente");
+	}
+	
+	
 	
 }
