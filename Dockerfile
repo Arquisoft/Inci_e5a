1c08203b68af85844909aac054292b0c2e495814
Miguel <miguelvilla997@gmail.com>
2018-06-15 18:20:57 +0200

    Cambios en kafka para que corra desde docker

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /usr/src/Inci_e5a
 COPY . /usr/src/Inci_e5a/
 RUN mvn package
 EXPOSE 8090
-CMD ["java", "-jar", "target/Inci_e5a-0.1.1.jar", "--spring.kafka.bootstrap-servers=kafka:9092","--spring.data.mongodb.uri = mongodb://admin:uo251836@ds147420.mlab.com:47420/sistema_incidencias"]
+CMD ["java", "-jar", "target/Inci_e5a-0.1.1.jar","--spring.kafka.bootstrap-servers=kafka:9092"]
--- src/main/java/asw/inciManager/kafkamanager/producers/KafkaConfiguration.java
+++ src/main/java/asw/inciManager/kafkamanager/producers/KafkaConfiguration.java
@@ -7,18 +7,20 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
+@EnableKafka
 public class KafkaConfiguration {
 	
     @Bean
     public Map<String, Object> producerConfigs() 
     {
     	Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "spring.kafka.bootstrap-servers:localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
